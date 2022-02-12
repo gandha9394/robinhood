4ebe7e29b2f0408064fb20376cd3f376f5d6a7c4
dhiraj0 <dhira@setu.co>
2022-02-12 15:13:37 +0530

    restructuring to have only core/ web/ rh/

--- rh/daemon/container.js
+++
@@ -1,6 +0,0 @@
-
-exports.startDockerContainer = (ptyProcess, image, memoryLimit, cpus, cpuShares) => {
-    let dockerRunCommand = `docker run -it --rm --privileged=true --memory="${memoryLimit}" --cpus="${cpus}" --cpus-shares="${cpuShares}" ${image} bash\r`
-    ptyProcess.write(dockerRunCommand);
-    return ptyProcess;
-}
--- rh/metrics/index.js
+++
@@ -1,9 +0,0 @@
-const { spawnShell } = require("../utils/pty")
-
-const ptyProcess = spawnShell()
-
-ptyProcess.onData(data => {
-    process.stdout.write(data);
-})
-
-ptyProcess.write(`docker stats --no-stream --format "{{ json . }}" | grep "rh-container"\r`)
